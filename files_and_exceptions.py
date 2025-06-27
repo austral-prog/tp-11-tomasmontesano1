ee978c083872b9edc2d69f8daa668973ae4872e8
tomasmontesano1 <timontesano@mail.austral.edu.ar>
2025-06-26 23:31:57 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,22 +1,22 @@
 def read_file_to_dict(datos):
     diccionario={}
-    with open(datos, 'r') as file:
+    
     try:
-        
-            linea = file.readline()
-            contador=0
-            for i in range(len(linea)):
-                if linea[i] == ";":
-                    venta = linea[contador:i]
-                    a = venta.find(":")
-                    if a != -1:
-                        clave = venta[:a]
-                        valor = float(venta[a+1:])
-                        if clave not in diccionario:
-                            diccionario[clave] = [valor]
-                        else:
-                            diccionario[clave].append(valor)
-                    contador = i+1
+        with open(datos, 'r') as file:
+        linea = file.readline()
+        contador=0
+        for i in range(len(linea)):
+            if linea[i] == ";":
+                venta = linea[contador:i]
+                a = venta.find(":")
+                if a != -1:
+                    clave = venta[:a]
+                    valor = float(venta[a+1:])
+                    if clave not in diccionario:
+                        diccionario[clave] = [valor]
+                    else:
+                        diccionario[clave].append(valor)
+                contador = i+1
         return diccionario
     except FileNotFoundError():
         raise (f"El archivo '{datos}' no existe")
